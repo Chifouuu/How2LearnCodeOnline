67377121a52c9b5fdcc11c51cb429464db7211aa
chifou <gregoirewild@gmail.com>
2023-04-19 18:06:14 +0200

    cathégories

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,14 @@ import { Routes, Route, NavLink } from 'react-router-dom';
 import { Home } from './screens/Home';
 import { Resources } from './screens/Rescources';
 import { Legal } from './screens/Legel';
+import { Ide } from './screens/resources/Ide';
 
 function App() {
   return (
     <div className="App">
       <header className="bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 shadow-2xl shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 ml-4 mr-4 mt-4 rounded-md">
         <h1 className='font-bold text-left text-white ml-4 mb-4 mt-4'>How2LearnCodeOnline</h1>
-        <nav className='bg-opacity-60 border-2 border-white flex items-center shadow-sm flex justify-center bg-white h-14  rounded-md'>
+        <nav className='bg-opacity-60 flex items-center shadow-sm flex justify-center bg-white h-14  rounded-md'>
           <NavLink className='mr-3' style={({ isActive }) => ({ fontWeight: isActive ? 'bold' : 'normal' })} to="/">Accueil</NavLink><br />
           <NavLink className="mr-3" style={({ isActive }) => ({ fontWeight: isActive ? 'bold' : 'normal' })} to="/resources">Resources</NavLink> <br />
           <NavLink className="mr-3" style={({ isActive }) => ({ fontWeight: isActive ? 'bold' : 'normal' })} to="/material">Materiel requis</NavLink> <br />
@@ -25,6 +26,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/legal' element={<Legal />} />
         <Route path='/resources' element={<Resources />} />
+        <Route path='/resources/ide' element={<Ide />} />
       </Routes>
       <footer>
         <p className="text-center text-black text-xs pt-8 text-base font-semibold leading-7 
--- src/screens/Rescources.tsx
+++ src/screens/Rescources.tsx
@@ -1,57 +1,7 @@
 export const Resources = () => {
     return (
-        <div className="mt-8">
-            <h2 className="font-bold mb-8 text-blue-500">Les IDE</h2>
-            <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl mb-8">
-                <div className="md:flex">
-                    <div className="md:shrink-0">
-                        <img className="h-48 w-full object-cover md:h-full md:w-48" src="/img/ij.png" alt="Modern building architecture" />
-                    </div>
-                    <div className="p-8">
-                        <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Ide Java</div>
-                        <a href="https://www.jetbrains.com/fr-fr/idea/download/#section=windows" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Intellij Idea</a>
-                        <p className="mt-2 text-slate-500">Excellent Ide combinant style, ergonomie, facilité d'utilisation et bien plus. Aujourd'hui Intellij Idea détrone largement NetBeans ainsi que Eclipse. Deux version vous sont disposé: IJ Community qui est gratuit et IJ Ultimate qui est payant.</p>
-                    </div>
-                </div>
-            </div>
-            <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl mb-8">
-                <div className="md:flex">
-                    <div className="md:shrink-0">
-                        <img className="h-48 w-full object-cover md:h-full md:w-48" src="/img/clion.png" alt="Modern building architecture" />
-                    </div>
-                    <div className="p-8">
-                        <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Ide C++</div>
-                        <a href="https://www.jetbrains.com/fr-fr/idea/download/#section=windows" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">CLion</a>
-                        <p className="mt-2 text-slate-500">Ide basé sous Intellij Idea, qui à été adapté au C++. C'est la version "premium" de Visual Studio. CLion est payant, mais est très pratique. Si vous voulez un IDE C++ Gratuit, téléchargez Visual Studio. </p>
-                    </div>
-                </div>
-            </div>
-            <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl mb-8">
-                <div className="md:flex">
-                    <div className="md:shrink-0">
-                        <img className="h-48 w-full object-cover md:h-full md:w-48" src="/img/vscode.png" alt="Modern building architecture" />
-                    </div>
-                    <div className="p-8">
-                        <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Ide dit "Web"</div>
-                        <a href="https://www.jetbrains.com/fr-fr/idea/download/#section=windows" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Visual Studio Code</a>
-                        <p className="mt-2 text-slate-500">Du même style que Visual Studio, VsCode en ont déjà conquis plus d'un ! C'est gratuit, bourée de plugins; themes. Si vous avez les moyens je vous conseille d'acheter Intellij Idea qui est aussi un Ide pour le Web, mais malheuresement vous devez avec la version "Ultimate" de Ij pour faire du web.</p>
-                    </div>
-                </div>
-            </div>
-            <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl mb-8">
-                <div className="md:flex">
-                    <div className="md:shrink-0">
-                        <img className="h-48 w-full object-cover md:h-full md:w-48" src="/img/pycharm.jpeg" alt="Modern building architecture" />
-                    </div>
-                    <div className="p-8">
-                        <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Ide Python</div>
-                        <a href="https://www.jetbrains.com/fr-fr/idea/download/#section=windows" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">PyCharm</a>
-                        <p className="mt-2 text-slate-500">Basée lui aussi sur Intellij Idea, est un Ide combinant style, ergonomie, facilité d'utilisation et bien plus. Avec un compilateur performant, cette ide vous fera gagner du temps, en plus il est 100% gratuit :) !</p>
-                    </div>
-                </div>
-            </div>
-
-
+        <div className="mt-8 place-content-center">
+            <a href="/resources/ide" className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 rounded-md pr-4 pl-4 pt-2 pb-2 shadow-2xl shadow-lg shadow-blue-500/50  hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300">Les IDE</a>
         </div>
     )
 }
